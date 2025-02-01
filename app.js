50e4bd6108cabc114364c36b5adc93d94eb6a9db
AlvaroGil93 <akolotronic93@hotmail.com>
2025-01-31 19:33:08 -0600

    actua

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ import { connectionMongo } from './src/config/dataBase.js';
 import usersRouter from './src/routes/user.routes.js';
 import loginRouter from './src/routes/login.routes.js';
 import cors from 'cors'; 
-
+//conf para el frotend
+import path from "path";
+import { fileURLToPath } from "url";
 // 2. configurar el uso de nuestro servidor y dependencias
 const app = express(); 
 dotenv.config(); 
@@ -14,13 +16,25 @@ connectionMongo();
 const port = process.env.PORT || 3001
 app.use(cors()); 
 
+//conf para el frontend
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // Le indico las rutas que debe utilizar
 app.use(express.json());
 app.use('/usuarios', usersRouter);
 app.use('/iniciarSesion', loginRouter);
 
-app.get('/', (req, res)=>{
-    res.send('Backed funcionando correctamente');
+
+//vamos hacer la peticion para que se muestre nuestro frontend
+// Servir archivos estáticos desde la carpeta "public"
+app.use(express.static(path.join(__dirname, "public")));
+
+// Ruta principal para servir index.html
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+
 export default app;
+
